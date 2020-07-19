732194d5364bd2422064b2abffe0b63ec6e08aac
ohno <ohno@ohnos-MacBook-Pro.local>
2020-07-19 13:10:37 -0500

    MOD: remove for mac ussage

--- polybar/scripts/close.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xdo kill -a "polybar-notify_eDP1"
-xdo kill -a "polybar-close_eDP1"
-polybar open &
--- polybar/scripts/open.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xdo kill -a "polybar-open_eDP1"
-polybar notify &
-polybar close & 
